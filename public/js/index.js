01f5898c4cf8fc4f43a9a8c887d39b622721f19d
Daeseang <eotmdzla@gmail.com>
2019-02-12 10:47:55 +0900

    login 구현

--- public/js/index.js
+++ public/js/index.js
@@ -7,3 +7,32 @@ var config = {
   messagingSenderId: "572731860757"
 };
 firebase.initializeApp(config);
+
+/***** 전역변수 설정 *****/
+var log = console.log;
+var auth = firebase.auth();
+var db = firebase.database();
+var googleAuth = new firebase.auth.GoogleAuthProvider();
+
+/***** Auth *****/
+$("#login_bt").on("click", function(){
+  auth.signInWithPopup(googleAuth);
+  //auth.signInWithRedirect(googleAuth);
+});
+$("#logout_bt").on("click", function(){
+  auth.signOut();
+});
+
+auth.onAuthStateChanged(function(result){
+  if(result) {
+    var email = '<img src="'+result.photoURL+'" style="width:24px;box-radius:50%;"> '+result.email;
+    $("#login_bt").hide();
+    $("#logout_bt").show();
+    $("#user_email").html(email);
+  }
+  else {
+    $("#login_bt").show();
+    $("#logout_bt").hide();
+    $("#user_email").html('');
+  }
+});
