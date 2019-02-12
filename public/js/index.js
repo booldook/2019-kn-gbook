7e83164b171711b87afc50cb99c6dcca7c056837
Daeseang <eotmdzla@gmail.com>
2019-02-12 09:47:11 +0900

    HTML 완성

---
+++ public/js/index.js
@@ -0,0 +1,9 @@
+var config = {
+  apiKey: "AIzaSyB10OGY0LJ8r9cg6dITLx4W1WjgY3F_m0E",
+  authDomain: "booldook-gbook.firebaseapp.com",
+  databaseURL: "https://booldook-gbook.firebaseio.com",
+  projectId: "booldook-gbook",
+  storageBucket: "booldook-gbook.appspot.com",
+  messagingSenderId: "572731860757"
+};
+firebase.initializeApp(config);
